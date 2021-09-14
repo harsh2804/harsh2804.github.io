3163f80ebcb4d7c32534379baa2773a09c5322a8
harsh2804 <harsh0233@gmail.com>
2021-09-15 04:38:25 +0530

    Update season_var.py

--- season_var.py
+++ season_var.py
@@ -140,7 +140,7 @@ table_with_export_buttons = pn.pane.HTML("<h1>hello</h1>",sizing_mode='stretch_w
 
 #file_download_csv = pn.widgets.FileDownload(filename="Statistical.csv", button_type="primary")   
 
-file_download = pn.widgets.FileDownload(file='figure.png', filename='figure.png',button_type='success',label='Download Plot',name='Click to download chart')
+file_download = pn.widgets.FileDownload(file='figure.png',button_type='success',label='Download',name='Click to download chart')
 
 @pn.depends(s.param.value,s1.param.value,s2.param.value,s3.param.value,s4.param.value)
 def p1(s,s1,s2,s3,s4):

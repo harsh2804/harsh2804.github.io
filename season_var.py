d29cdaa0b359796d45d30cb6c1f5dcc7517d99b7
harsh2804 <harsh0233@gmail.com>
2021-09-15 04:22:32 +0530

    Update season_var.py

--- season_var.py
+++ season_var.py
@@ -463,7 +463,7 @@ bootstrap.main.append(pn.Column(file_download,pn.Card(p1,width = 100),pn.Row(pn.
 
 #bootstrap.servable(title="Seasonal Variation");
 
-pn.template.FastListTemplate(header='https://panel.holoviz.org/_static/logo_horizontal.png',   title="Seasonal Variation", 
+pn.template.FastListTemplate(header=pn.panel('https://panel.holoviz.org/_static/logo_horizontal.png',height=40),   title="Seasonal Variation", 
                             sidebar = [s4,s,s1,s2,s3,text1],
                             main = [pn.Column(pn.Card(file_download,p1,sizing_mode = 'stretch_both'),pn.Row(pn.Card(table_with_export_buttons,title='Statistical Table'),p2))]).servable();
 

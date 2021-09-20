fd1c7655ae31cdd651ba4bdbe99e09feb9cf44c2
harsh2804 <harsh0233@gmail.com>
2021-09-20 08:10:26 +0530

    Update season_var.py

--- season_var.py
+++ season_var.py
@@ -96,7 +96,7 @@ height = im1.size[1]
 width = im1.size[0]
 im1 = np.array(im1).astype(np.float) / 255
 
-
+'''
 f = Figure(figsize=(12,9),dpi = 200)
 #cw = os.getcwd()
 #f.bbox.xmax-width
@@ -105,7 +105,7 @@ ax =f.subplots()
 #print(height)      
 ax.figure.figimage(im1, f.bbox.xmax-width, f.bbox.ymax-height)      
 FigureCanvas(f)
-
+'''
 
 
 title = '## hello'
@@ -271,7 +271,7 @@ def p1(s,s1,s2,s3,s4):
  y1 = round(c['rain'][m1],2)
  # = pn.widgets.DataFrame(c)   
  #print(c)
- '''
+ 
  f = Figure(figsize=(12,9),dpi = 200)
  #cw = os.getcwd()
  
@@ -281,7 +281,7 @@ def p1(s,s1,s2,s3,s4):
  #print(height)      
  ax.figure.figimage(im1, f.bbox.xmax-width, f.bbox.ymax-height)      
  FigureCanvas(f)
- '''
+ 
  db1 = datetime.datetime.strptime(str(s2),"%m")
  m1 = db1.strftime("%b")
  db2 = datetime.datetime.strptime(str(s3),"%m")

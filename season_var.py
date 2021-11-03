f76d8f5b63fa9899e3fe82c2f0c909b8f4414552
harsh2804 <harsh0233@gmail.com>
2021-11-03 13:05:11 +0530

    Update season_var.py

--- season_var.py
+++ season_var.py
@@ -559,11 +559,13 @@ def p1(s,s1,s2,s3,s4,s5,radio_group):
  c= getdata()
  import calendar
  c= c[c.name == s4]
- c['Month'] = c['month'].apply(lambda x: calendar.month_abbr[x])  
- c= c.replace(-99.9,np.nan)
+ c['Month'] = c['month'].apply(lambda x: calendar.month_abbr[x])
+ c = c[c.rain >= 0]
+ #c= c.replace(-99.9,np.nan)
  c['year-month']= c['year'].astype(str) + '-' +  c['Month']
- fig= px.line(c, x="year-month", y="rain",title='data visualization(monthly) of '+ s4)#, color='Month')#, symbol="Month")
- fig.update_layout(xaxis_type='category')      
+ c = c.reset_index()      
+ fig= px.line(c, x="year-month", y="rain",title='Available Monthly  data of '+ s4)#, color='Month')#, symbol="Month")
+ fig.update_layout(xaxis_type='category',title_x=0.5)      
  #fig = px.bar(c, x="ym", y="rain", color="Month", title='monthly dataset of '+ s4) 
  fig.layout.autosize = True
  matpl.object = fig
